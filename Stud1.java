59799bc4e626414cb9f8b871eeb946979d7c5e9c
Nikhil Jais <97941600+Nikhiljaisajce@users.noreply.github.com>
2022-04-22 13:00:24 +0530

    Add files via upload

---
+++ Stud1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Stud1{
+	public static void main(String args[])
+	{
+	String name;
+	System.out.println("Enter your name");
+	Scanner obj1=new Scanner(System.in);
+	name=obj1.nextLine();
+	System.out.println(name);
+}
+}
