d80baa1c9d23d25a8e5c0cb3cd4c1852f647bb8a
Nikhil Jais <97941600+Nikhiljaisajce@users.noreply.github.com>
2022-05-18 11:53:03 +0530

    Add files via upload

---
+++ Search_Element.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class Search_Element
+{
+    public static void main(String[] args) 
+    {
+        int n, x, flag = 0, i = 0;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter no. of elements you want in array:");
+        n = s.nextInt();
+        int a[] = new int[n];
+        System.out.println("Enter all the elements:");
+        for(i = 0; i < n; i++)
+        {
+            a[i] = s.nextInt();
+        }
+        System.out.print("Enter the element you want to find:");
+        x = s.nextInt();
+        for(i = 0; i < n; i++)
+        {
+            if(a[i] == x)
+            {
+                flag = 1;
+                break;
+            }
+            else
+            {
+                flag = 0;
+            }
+        }
+        if(flag == 1)
+        {
+            System.out.println("Element found at position:"+(i + 1));
+        }
+        else
+        {
+            System.out.println("Element not found");
+        }
+    }
+}						
+		        
+
