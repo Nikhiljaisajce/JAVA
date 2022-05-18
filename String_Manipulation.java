82eae2188e52444a048a18a5b5c29c728aaaca74
Nikhil Jais <97941600+Nikhiljaisajce@users.noreply.github.com>
2022-05-18 11:55:51 +0530

    Add files via upload

---
+++ String_Manipulation.java
@@ -0,0 +1,11 @@
+class String_Manipulation {
+  public static void main(String[] args) {
+
+    String greet = "Welcome to Java";
+    System.out.println("String: " + greet);
+
+    // get the length of greet
+    int length = greet.length();
+    System.out.println("Length: " + length);
+  }
+}
